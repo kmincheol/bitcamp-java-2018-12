70a7f9cf1a0ce0108dd4c58c7fccfd387d86ce53
kmincheol <mincheol.kmc@gmail.com>
2019-01-04 17:52:27 +0900

    리터럴, 콘솔출력, 변수선언, 키보드입력

--- java-project/src/main/java/bitcamp/lms/App.java
+++ java-project/src/main/java/bitcamp/lms/App.java
@@ -3,14 +3,14 @@ package bitcamp.lms;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("번호: " + 1);
-    System.out.println("수업명: 자바 프로젝트 실습");
-    System.out.println("설명: " + "자바 프로젝트를 통한 자바 언어 활용법 익히기");
-    System.out.print("시작일: ");
-    System.out.println("2019-01-02");
-    System.out.println("종료일: 2019-05-28");
-    System.out.print("총수업시간: 1000 시간");
-    System.out.println();
-    System.out.printf("일수업시간: %d 시간", 8);
+    java.util.Scanner keyboard = new java.util.Scanner(System.in);
+
+    System.out.print("수업명? ");
+    String className = keyboard.nextInt();
+
+    System.out.print("이름? ");
+    String name= keyboard.next();
+
+    System.out.printf("%d(%s)\n", age, name);
   }
 }
---
+++ java-project/src04/main/java/bitcamp/lms/App2.java
@@ -0,0 +1,14 @@
+package bitcamp.lms;
+
+public class App2 {
+
+  public static void main(String[] args) {
+    System.out.printf("번호: %d\n",1);
+    System.out.printf("이름: %s\n", "홍길동");
+    System.out.printf("이메일: %s\n", "hong@test.com");
+    System.out.printf("암호: %s\n","1111");
+    System.out.println("사진: hong.png");
+    System.out.printf("전화: 1111-2222\n");
+    System.out.printf("가입일: 2019-01-01\n");
+  }
+}

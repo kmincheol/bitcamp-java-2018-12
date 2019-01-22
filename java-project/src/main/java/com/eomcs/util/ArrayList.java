cae84705845633979ecce2a804d09d826daa4bdb
kmincheol <mincheol.kmc@gmail.com>
2019-01-22 17:59:39 +0900

    .

--- java-project/src/main/java/com/eomcs/util/ArrayList.java
+++ java-project/src/main/java/com/eomcs/util/ArrayList.java
@@ -1,83 +1,98 @@
 package com.eomcs.util;
 
 import java.util.Arrays;
-import com.eomcs.lms.domain.Board;
 
 public class ArrayList<E> {
-  static final int DEFAULT_CAPACITY = 10;
-  Object[] list;
-  int size = 0;
-
+  private static final int DEFAULT_CAPACITY = 10;
+  private Object[] list;
+  private int size = 0;
+  
   public ArrayList() {
-    list = new Object[DEFAULT_CAPACITY];
+    list  = new Object[DEFAULT_CAPACITY];
   }
-
-  public ArrayList(E[] arr, int initialCapacity) {
-    if (initialCapacity > DEFAULT_CAPACITY)
-      list = new Object[DEFAULT_CAPACITY];
+  
+  public ArrayList(int initialCapacity) {
+    if (initialCapacity > DEFAULT_CAPACITY) 
+      list = new Object[initialCapacity];
     else
       list = new Object[DEFAULT_CAPACITY];
   }
-
+  
   @SuppressWarnings("unchecked")
-  public E[] toArray(E[] sampleArr) {
-    return (E[]) Arrays.copyOf(list, size, sampleArr.getClass());
-    /*
-     * E[] arr = Arrays.copyOf(sampleArr, size); for (int i = 0; i < size; i++) { arr[i] = (E)
-     * list[i]; } return arr;
-     */
+  public E[] toArray(E[] a) {
+    if (a.length < size) {
+      return (E[]) Arrays.copyOf(list, size, a.getClass());
+    }
+    System.arraycopy(list, 0, a, 0, size);
+    if (a.length > size)
+      a[size] = null;
+    return a;
   }
-
+  
   public void add(E obj) {
     if (size >= list.length) {
       int oldCapacity = list.length;
       int newCapacity = oldCapacity + (oldCapacity >> 1);
       list = Arrays.copyOf(list, newCapacity);
     }
-
+    
     list[size++] = obj;
   }
 
   @SuppressWarnings("unchecked")
   public E get(int index) {
-    // index : 값을 꺼낼 배열의 항목 위치
-    if (index < 0 || index >= size)
+    if (index < 0 || index >= size) 
       return null;
+    
     return (E) list[index];
   }
 
-  @SuppressWarnings("unchecked")
-  public E set(int index, E value) {
-    // index : 값을 변경할 배열의 항목 위치
-    // value : 해당 위치에 있는 값을 대체할 값
-    // 리턴 값 : 대체되기 전의 기존 값
+  public E set(int index, E obj) {
     if (index < 0 || index >= size)
       return null;
-
-    E obj = (E) list[index];
-    list[index] = value;
-    return obj;
+    
+    @SuppressWarnings("unchecked")
+    E old = (E)list[index];
+    list[index] = obj;
+    return old;
   }
-
-  @SuppressWarnings("unchecked")
+  
   public E remove(int index) {
-    // index : 삭제할 배열의 항목 위치
-    // 리턴값 : 삭제된 이전 값
-    // 힌트 : System.arraycopy() 참고
     if (index < 0 || index >= size)
       return null;
-
-    E obj = (E) list[index];
-    for (int i = index; i < size - 1; i++) {
-      list[i] = list[i + 1];
-      size--;
-    }
-    return obj;
+    
+    @SuppressWarnings("unchecked")
+    E old = (E)list[index];
+    
+    int newSize = size - 1;
+    System.arraycopy(list, index + 1, list, index, newSize - index);
+    list[size = newSize] = null;
+    return old;
   }
-
+  
   public int size() {
-    return this.size;
+    return size;
+  }
+  
+  public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    
+    list.add("aaa");
+    list.add("bbb");
+    list.add("ccc");
+    list.add("ddd");
+    list.add("eee");
+    list.add("fff");
+    list.add("ggg");
+    
+    System.out.println(list.size());
+    
+    System.out.println(list.remove(3));
+    
+    System.out.println(list.size());
+    
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
   }
 }
-
-

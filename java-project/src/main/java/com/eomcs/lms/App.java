974b0683d699b648f9181c81c623ee944e2a5ed0
kmincheol <mincheol.kmc@gmail.com>
2019-01-28 18:22:48 +0900

    .

--- java-project/src/main/java/com/eomcs/lms/App.java
+++ java-project/src/main/java/com/eomcs/lms/App.java
@@ -4,17 +4,27 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Stack;
 import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.domain.Member;
 import com.eomcs.lms.handler.BoardAddCommand;
+import com.eomcs.lms.handler.BoardDeleteCommand;
 import com.eomcs.lms.handler.BoardDetailCommand;
-import com.eomcs.lms.handler.BoardHandler;
 import com.eomcs.lms.handler.BoardListCommand;
+import com.eomcs.lms.handler.BoardUpdateCommand;
 import com.eomcs.lms.handler.Command;
-import com.eomcs.lms.handler.LessonHandler;
-import com.eomcs.lms.handler.MemberHandler;
+import com.eomcs.lms.handler.LessonAddCommand;
+import com.eomcs.lms.handler.LessonDeleteCommand;
+import com.eomcs.lms.handler.LessonDetailCommand;
+import com.eomcs.lms.handler.LessonListCommand;
+import com.eomcs.lms.handler.LessonUpdateCommand;
+import com.eomcs.lms.handler.MemberAddCommand;
+import com.eomcs.lms.handler.MemberDeleteCommand;
+import com.eomcs.lms.handler.MemberDetailCommand;
+import com.eomcs.lms.handler.MemberListCommand;
+import com.eomcs.lms.handler.MemberUpdateCommand;
 
 public class App {
 
@@ -27,6 +37,8 @@ public class App {
   public static void main(String[] args) {
 
     ArrayList<Board> boardList = new ArrayList<>();
+    ArrayList<Member> memberList = new ArrayList<>();
+    ArrayList<Lesson> lessonList = new ArrayList<>();
     HashMap<String, Command> commandMap = new HashMap<>();
 
     // 핸들러가 필요로 하는 의존 객체를 이 클래스에서 만들어 주입해 준다.
@@ -35,24 +47,28 @@ public class App {
     commandMap.put("/board/add", new BoardAddCommand(keyboard, boardList));
     commandMap.put("/board/list", new BoardListCommand(keyboard, boardList));
     commandMap.put("/board/detail", new BoardDetailCommand(keyboard, boardList));
-
-    LessonHandler lessonHandler = new LessonHandler(keyboard, new ArrayList<>());
-    MemberHandler memberHandler = new MemberHandler(keyboard, new ArrayList<>());
-    BoardHandler boardHandler = new BoardHandler(keyboard, new LinkedList<>());
+    commandMap.put("/board/update", new BoardUpdateCommand(keyboard, boardList));
+    commandMap.put("/board/delete", new BoardDeleteCommand(keyboard, boardList));
+    commandMap.put("/member/add", new MemberAddCommand(keyboard, memberList));
+    commandMap.put("/member/list", new MemberListCommand(keyboard, memberList));
+    commandMap.put("/member/detail", new MemberDetailCommand(keyboard, memberList));
+    commandMap.put("/member/update", new MemberUpdateCommand(keyboard, memberList));
+    commandMap.put("/member/delete", new MemberDeleteCommand(keyboard, memberList));
+    commandMap.put("/lesson/add", new LessonAddCommand(keyboard, lessonList));
+    commandMap.put("/lesson/list", new LessonListCommand(keyboard, lessonList));
+    commandMap.put("/lesson/detail", new LessonDetailCommand(keyboard, lessonList));
+    commandMap.put("/lesson/update", new LessonUpdateCommand(keyboard, lessonList));
+    commandMap.put("/lesson/delete", new LessonDeleteCommand(keyboard, lessonList));
 
     while (true) {
       String command = prompt();
-
       // 사용자가 입력한 명령을 스택에 보관한다.
       commandHistory.push(command);
-
       // 사용자가 입력한 명령을 큐에 보관한다.
       commandHistory2.offer(command);
-
       if (command.equals("quit")) {
         System.out.println("안녕!");
         break;
-
       } else if (command.equals("history")) {
         printCommandHistory(new Iterator<String>() {
           int index = commandHistory.size() - 1;
@@ -67,10 +83,8 @@ public class App {
             return commandHistory.get(index--);
           }
         });
-
       } else if (command.equals("history2")) {
         printCommandHistory(commandHistory2.iterator());
-
       } else {
         Command commandHandler = commandMap.get(command);
 
@@ -79,10 +93,8 @@ public class App {
         else
           commandHandler.execute();
       }
-
       System.out.println(); // 결과 출력 후 빈 줄 출력
     }
-
     keyboard.close();
   }
 

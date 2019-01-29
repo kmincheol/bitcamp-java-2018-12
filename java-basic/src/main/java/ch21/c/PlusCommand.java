7229562f5abe244759f97c2316519003e6e32df6
kmincheol <mincheol.kmc@gmail.com>
2019-01-29 18:31:02 +0900

    .

---
+++ java-basic/src/main/java/ch21/c/PlusCommand.java
@@ -0,0 +1,23 @@
+package ch21.c;
+
+import java.util.Scanner;
+
+public class PlusCommand implements Command {
+
+  public Scanner keyboard;
+
+  public PlusCommand(Scanner keyboard) {
+    this.keyboard = keyboard;
+  }
+
+  @Override
+  public void execute() {
+    System.out.print("값1? ");
+    int a = Integer.parseInt(keyboard.nextLine());
+    System.out.print("값2? ");
+    int b = Integer.parseInt(keyboard.nextLine());
+
+    System.out.printf("%d + %d = %d\n", a, b, (a + b));
+
+  }
+}

a284e8191170d656ef6378319cc3b990f98a1e77
kmincheol <mincheol.kmc@gmail.com>
2019-02-13 19:37:44 +0900

    .

--- java-project-client/src/main/java/com/eomcs/lms/handler/BoardDeleteCommand.java
+++ java-project-client/src/main/java/com/eomcs/lms/handler/BoardDeleteCommand.java
@@ -1,40 +1,41 @@
 package com.eomcs.lms.handler;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 import java.util.Scanner;
 import com.eomcs.lms.domain.Board;
 
 public class BoardDeleteCommand implements Command {
-  
+
   Scanner keyboard;
-  List<Board> list;
-  
-  public BoardDeleteCommand(Scanner keyboard, List<Board> list) {
+
+  public BoardDeleteCommand(Scanner keyboard) {
     this.keyboard = keyboard;
-    this.list = list;
   }
 
   @Override
-  public void execute() {
+  public void execute(ObjectInputStream in, ObjectOutputStream out) {
     System.out.print("번호? ");
     int no = Integer.parseInt(keyboard.nextLine());
 
-    int index = indexOfBoard(no);
-    if (index == -1) {
-      System.out.println("해당 게시글을 찾을 수 없습니다.");
-      return;
+    try {
+      out.writeUTF("/board/delete");
+      out.flush();
+      if (!in.readUTF().equals("OK"))
+        throw new Exception("서버에서 해당 명령어를 처리하지 못합니다.");
+
+      out.writeInt(no);
+      out.flush();
+
+      String status = in.readUTF();
+      if (status.equals("OK"))
+        System.out.println("서버에서 데이터 삭제 성공");
+      else
+        System.out.println("서버에서 데이터 삭제 실패");
+
+    } catch (Exception e) {
+
     }
-    
-    list.remove(index);
-    
-    System.out.println("게시글을 삭제했습니다.");
-  }
-  
-  private int indexOfBoard(int no) {
-    for (int i = 0; i < list.size(); i++) {
-      Board b = list.get(i);
-      if (b.getNo() == no)
-        return i;
-    }
-    return -1;
   }
 }
--- java-project-server/src/main/java/com/eomcs/lms/ServerApp.java
+++ java-project-server/src/main/java/com/eomcs/lms/ServerApp.java
@@ -1,4 +1,4 @@
-// 2단계 : 클라이언트 연결을 승인한다.
+// 11단계 : 서비스 클래스의 일반화(상속)를 수행한다.
 package com.eomcs.lms;
 
 import java.io.ObjectInputStream;
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import com.eomcs.lms.domain.Board;
 import com.eomcs.lms.domain.Lesson;
 import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.BoardService;
+import com.eomcs.lms.service.LessonService;
+import com.eomcs.lms.service.MemberService;
 
 public class ServerApp {
   static ArrayList<Member> members = new ArrayList<>();
@@ -17,40 +20,63 @@ public class ServerApp {
   static ObjectInputStream in;
   static ObjectOutputStream out;
 
+  static MemberService memberService = null;
+  static LessonService lessonService = null;
+  static BoardService boardService = null;
+
   public static void main(String[] args) {
     try (ServerSocket serverSocket = new ServerSocket(8888)) {
       System.out.println("서버 시작!");
+
+      try {
+        memberService = new MemberService();
+        memberService.loadData("member.bin");
+      } catch (Exception e) {
+        System.out.println("회원 데이터 로딩 중 오류 발생!");
+        // e.printStackTrace();
+      }
+      try {
+        lessonService = new LessonService();
+        lessonService.loadData("lesson.bin");
+      } catch (Exception e) {
+        System.out.println("수업 데이터 로딩 중 오류 발생!");
+        // e.printStackTrace();
+      }
+      try {
+        boardService = new BoardService();
+        boardService.loadData("board.bin");
+      } catch (Exception e) {
+        System.out.println("게시물 데이터 로딩 중 오류 발생!");
+        // e.printStackTrace();
+      }
 
       while (true) {
         try (Socket socket = serverSocket.accept();
             ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
 
+          boardService.init(in, out);
+          lessonService.init(in, out);
+          memberService.init(in, out);
+
           System.out.println("클라이언트와 연결되었음!");
-          members.clear();
-          lessons.clear();
-          boards.clear();
+//          members.clear();
+//          lessons.clear();
+//          boards.clear();
 
           ServerApp.in = in;
           ServerApp.out = out;
-
-          MemberCommand.in = in;
-          MemberCommand.out = out;
-          LessonCommand.in = in;
-          LessonCommand.out = out;
-          BoardCommand.in = in;
-          BoardCommand.out = out;
 
           loop: while (true) {
             String request = in.readUTF();
             System.out.println(request);
 
             if (request.startsWith("/member/")) {
-              MemberCommand.service(request);
+              memberService.execute(request);
             } else if (request.startsWith("/lesson/")) {
-              LessonCommand.service(request);
+              lessonService.execute(request);
             } else if (request.startsWith("/board/")) {
-              BoardCommand.service(request);
+              boardService.execute(request);
             } else if (request.equals("quit")) {
               quit();
               break loop;
@@ -69,6 +95,24 @@ public class ServerApp {
   }
 
   static void quit() throws Exception {
+    try {
+      boardService.saveData();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      // e.printStackTrace();
+    }
+    try {
+      lessonService.saveData();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      // e.printStackTrace();
+    }
+    try {
+      memberService.saveData();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      // e.printStackTrace();
+    }
     out.writeUTF("종료함!");
     out.flush();
   }

9b70620190c857a8f855ad06a40714df11a47ef6
kmincheol <mincheol.kmc@gmail.com>
2019-02-18 17:30:23 +0900

    .

--- java-basic/src/main/java/ch24/a/Test02.java
+++ java-basic/src/main/java/ch24/a/Test02.java
@@ -1,6 +1,8 @@
-package ch24;
+// 멀티 스레드 적용 후 
+package ch24.a;
 
-public class Test01 {
+public class Test02 {
+
   static class MyThread extends Thread {
     @Override
     public void run() {
@@ -11,17 +13,19 @@ public class Test01 {
       }
     }
   }
-
+  
   public static void main(String[] args) {
-
+    
     // 스레드는 비동기로 동작한다.
     // 스레드에 작업을 시킨 후, 그 스레드가 작업이 끝날 때까지 기다리지 않고 즉시 리턴한다.
-    // 따라서 스레드의 작업과 main()의 코드가 병행(concurrent)으로 실행한다.
+    // 따라서 스레드이 작업과 main()의 코드가 병행(concurrent)으로 실행한다.
     MyThread t = new MyThread();
     t.start();
-
+    
     for (int i = 0; i < 1000; i++) {
       System.out.println(">>>>> " + i);
     }
+
   }
+
 }

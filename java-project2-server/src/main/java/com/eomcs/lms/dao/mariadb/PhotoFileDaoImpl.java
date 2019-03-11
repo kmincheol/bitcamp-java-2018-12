fcab81169525b3383aa6b330d6d9280ad12b5940
kmincheol <mincheol.kmc@gmail.com>
2019-03-11 18:34:28 +0900

    .

--- java-project2-server/src/main/java/com/eomcs/lms/dao/mariadb/PhotoFileDaoImpl.java
+++ java-project2-server/src/main/java/com/eomcs/lms/dao/mariadb/PhotoFileDaoImpl.java
@@ -8,36 +8,31 @@ import com.eomcs.lms.domain.PhotoFile;
 
 public class PhotoFileDaoImpl implements PhotoFileDao {
 
-  //Mybatis 의존 객체 선언
+  // Mybatis 의존 객체 선언
   SqlSessionFactory sqlSessionFactory;
 
   public PhotoFileDaoImpl(SqlSessionFactory sqlSessionFactory) {
     this.sqlSessionFactory = sqlSessionFactory;
   }
-  
+
   @Override
   public List<PhotoFile> findByPhotoBoardNo(int photoBoardNo) {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-      return sqlSession.selectList(
-          "PhotoFileMapper.findByPhotoBoardNo", photoBoardNo);
+      return sqlSession.selectList("PhotoFileMapper.findByPhotoBoardNo", photoBoardNo);
     }
   }
 
   @Override
-  public void insert(PhotoFile photoFile) {
+  public void insert(List<PhotoFile> photoFiles) {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-      sqlSession.insert("PhotoFileMapper.insert", photoFile);
-      sqlSession.commit();
+      sqlSession.insert("PhotoFileMapper.insert", photoFiles);
     }
   }
 
   @Override
   public int deleteByPhotoBoardNo(int photoBoardNo) {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-      int count = sqlSession.delete(
-          "PhotoFileMapper.deleteByPhotoBoardNo", photoBoardNo);
-      sqlSession.commit();
-      return count;
+      return sqlSession.delete("PhotoFileMapper.deleteByPhotoBoardNo", photoBoardNo);
     }
   }
 

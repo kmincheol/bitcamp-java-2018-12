a665136d3b86246b8c75a14b0ca12180f097c7a6
kmincheol <mincheol.kmc@gmail.com>
2019-03-29 18:32:09 +0900

    .

--- java-web-project/src/main/java/com/eomcs/lms/servlet/BoardListServlet.java
+++ java-web-project/src/main/java/com/eomcs/lms/servlet/BoardListServlet.java
@@ -19,16 +19,18 @@ public class BoardListServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
+    
     // Spring IoC 컨테이너에서 BoardService 객체를 꺼낸다.
     BoardService boardService = ServerApp.iocContainer.getBean(BoardService.class);
 
     List<Board> boards = boardService.list();
+    
     response.setContentType("text/html;charset=UTF-8");
+    
     PrintWriter out = response.getWriter();
     out.println("<html><head><title>게시물 목록</title></head>");
     out.println("<body><h1>게시물 목록</h1>");
-    out.println("<p><a href='form'>새 글</a></p>");
+    out.println("<p><a href='add'>새 글</a></p>");
     out.println("<table border='1'>");
     out.println("<tr> <th>번호</th> <th>제목</th> <th>등록일</th> <th>조회수</th> </tr>");
     for (Board board : boards) {

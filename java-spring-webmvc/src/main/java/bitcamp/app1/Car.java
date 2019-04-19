2f1ba1599a4c2916ea06ac66d05ea3951a2fbaa7
kmincheol <mincheol.kmc@gmail.com>
2019-04-19 17:54:12 +0900

    .

--- java-spring-webmvc/src/main/java/bitcamp/app1/Car.java
+++ java-spring-webmvc/src/main/java/bitcamp/app1/Car.java
@@ -7,61 +7,50 @@ public class Car {
   protected String maker;
   protected boolean auto;
   protected int capacity;
-  protected Date createDate;
+  protected Date createdDate;
   protected Engine engine;
-
+  
   @Override
   public String toString() {
     return "Car [model=" + model + ", maker=" + maker + ", auto=" + auto + ", capacity=" + capacity
-        + ", createDate=" + createDate + ", engine=" + engine + "]";
+        + ", createdDate=" + createdDate + ", engine=" + engine + "]";
   }
-
   public String getModel() {
     return model;
   }
-
   public void setModel(String model) {
     this.model = model;
   }
-
   public String getMaker() {
     return maker;
   }
-
   public void setMaker(String maker) {
     this.maker = maker;
   }
-
   public boolean isAuto() {
     return auto;
   }
-
-  public Engine getEngine() {
-    return engine;
-  }
-
-  public void setEngine(Engine engine) {
-    this.engine = engine;
-  }
-
   public void setAuto(boolean auto) {
     this.auto = auto;
   }
-
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
   public int getCapacity() {
     return capacity;
   }
-
   public void setCapacity(int capacity) {
     this.capacity = capacity;
   }
-
-  public Date getCreateDate() {
-    return createDate;
+  public Engine getEngine() {
+    return engine;
   }
-
-  public void setCreateDate(Date createDate) {
-    this.createDate = createDate;
+  public void setEngine(Engine engine) {
+    this.engine = engine;
   }
-
+  
+  
 }
